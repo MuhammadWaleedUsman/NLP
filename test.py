47576477710eab1eadcdf0cfa6aac7efa87b2583
Muhammad Waleed Usman <69990277+MuhammadWaleedUsman@users.noreply.github.com>
2020-10-04 16:10:57 +0500

    Mainfile

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
-print('hello')
-print('hello there')
-print('waleed')
-print('here')
+
+from urduhack.tokenization import word_tokenizer
+text = "عراق اور شام نے اعلان کیا ہے دونوں ممالک جلد اپنے اپنے سفیروں کو واپس بغداد اور دمشق بھیج دیں گے؟"
+words = word_tokenizer(text)
+
+print(words)
+
+
+
